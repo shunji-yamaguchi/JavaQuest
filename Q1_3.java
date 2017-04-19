a6f40c501264f687ad43ff9e652262b8d8ab35cd
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-19 17:43:12 +0900

    第一章の課題２つ

---
+++ Q1_3.java
@@ -0,0 +1,14 @@
+//次の文章を表示するプログラムKantan.javaを作ってください
+//
+//意外に"Java"って簡単だ。
+//意外に"Java"って簡単だ。
+//意外に"Java"って簡単だ。
+
+
+public class Q1_3{
+  public static void main(String[] args){
+    for(int i=0; i<3; i++){
+      System.out.println("意外と\"Java\"って簡単だ。");
+    }
+  }
+}
---
+++ Q1_4.java
@@ -0,0 +1,17 @@
+//例題2のAisatu.javaを修正して、System.out.printlnを一度しか使わずに
+//同じ実行結果になるようにしてください。
+//Aisatu.java
+//pabric class Aisatu{
+//  public staic void main(String[] args){
+//    System.out.println("おはよう。);
+//    System.out.println("こんにちは。"):
+//    System.out,println("こんばんは。");
+//  {
+//}
+
+
+public class Q1_4{
+  public static void main(String[] args){
+    System.out.println("おはよう。\nこんにちは。\nこんばんは。\n");
+  }
+}
