d61e60983492b4b00a68e13c371e8c0990da277b
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-20 13:27:48 +0900

    Q1_4 コメント文の修正

--- Q1_4.java
+++ Q1_4.java
@@ -1,12 +1,12 @@
 //例題2のAisatu.javaを修正して、System.out.printlnを一度しか使わずに
 //同じ実行結果になるようにしてください。
 //Aisatu.java
-//pabric class Aisatu{
-//  public staic void main(String[] args){
-//    System.out.println("おはよう。);
-//    System.out.println("こんにちは。"):
-//    System.out,println("こんばんは。");
-//  {
+//pablic class Aisatu{
+//  public static void main(String[] args){
+//    System.out.println("おはよう。");
+//    System.out.println("こんにちは。");
+//    System.out.println("こんばんは。");
+//  }
 //}
 
 
