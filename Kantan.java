b8a47674b3572a3fd0ba45ae8138b9b96ecea823
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-20 16:04:13 +0900

    Kantan.java インデントの修正

--- Kantan.java
+++ Kantan.java
@@ -4,11 +4,10 @@
 //意外に"Java"って簡単だ。
 //意外に"Java"って簡単だ。
 
-
 public class Kantan {
-  public static void main(String[] args) {
-    for (int i = 0; i < 3; i++) {
-      System.out.println("意外に\"Java\"って簡単だ。");
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("意外に\"Java\"って簡単だ。");
+        }
     }
-  }
 }
