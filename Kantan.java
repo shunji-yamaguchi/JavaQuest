1b389fa5deacc0a9757d0245241283bb28e00a93
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-20 14:17:27 +0900

    Kantan.javaの修正　空白(スペース)の追加

--- Kantan.java
+++ Kantan.java
@@ -7,8 +7,8 @@
 
 public class Kantan{
   public static void main(String[] args){
-    for(int i = 0; i < 3; i++){
-      System.out.println("意外に\"Java\"って簡単だ。");
+    for (int i=0; i<3; i++){
+      System. out. println("意外に\"Java\"って簡単だ。");
     }
   }
 }
