a1975e796a5617e477c2ae2fe110e7f2e80b55e2
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-20 11:52:18 +0900

    ファイル名の訂正 Q1-3.java -> Kantan.java

--- Kantan.java
+++ Kantan.java
@@ -5,7 +5,7 @@
 //意外に"Java"って簡単だ。
 
 
-public class Q1_3{
+public class Kantan{
   public static void main(String[] args){
     for(int i=0; i<3; i++){
       System.out.println("意外に\"Java\"って簡単だ。");
