08af97b06c669345abf9beecdeb83c745e39d28c
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-20 14:55:44 +0900

    Kantan.java 空白の修正

--- Kantan.java
+++ Kantan.java
@@ -5,9 +5,9 @@
 //意外に"Java"って簡単だ。
 
 
-public class Kantan{
-  public static void main( String[] args ){
-    for( int i=0; i<3; i++ ){
+public class Kantan {
+  public static void main(String[] args) {
+    for (int i=0; i<3; i++ ) {
       System.out.println("意外に\"Java\"って簡単だ。");
     }
   }
