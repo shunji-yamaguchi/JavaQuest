ce57cbcf2371ccb8313777a5cdfbf365078f42bb
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-21 14:25:40 +0900

    Q3_2.java MAX_NUMBER_OF_PEOPLEのスペルミスの修正

--- Q3_2.java
+++ Q3_2.java
@@ -6,20 +6,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Q3_2 {
-    private static final int MAX_NUMBER_OFPEAPLE = 2;
+
+    private static final int MAX_NUMBER_OF_PEOPLE = 2;
+
     public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         try {
             int sum = 0;
 
-            for (int count = 0; count < MAX_NUMBER_OFPEAPLE; count++) {
+            for (int count = 0; count < MAX_NUMBER_OF_PEOPLE; count++) {
                 System.out.println((count + 1) + "人目の名前を入力してください");
                 String name = reader.readLine();
                 System.out.println(name + "さんの年齢を入力してください。");
                 sum += Integer.parseInt(reader.readLine());
             }
-            System.out.println(MAX_NUMBER_OFPEAPLE + "人の平均年齢は" + ((double) sum / MAX_NUMBER_OFPEAPLE) + "です。");
+            System.out.println(MAX_NUMBER_OF_PEOPLE + "人の平均年齢は" + ((double) sum / MAX_NUMBER_OF_PEOPLE) + "です。");
         } catch (IOException e) {
             System.out.println(e);
         } catch (NumberFormatException e) {
