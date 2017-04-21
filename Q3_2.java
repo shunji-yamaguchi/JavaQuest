586c9409474eb6e867f6ec6228a7e7578615ae53
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-04-21 11:21:18 +0900

    Q3_1.java 単位の追加

--- Q3_2.java
+++ Q3_2.java
@@ -1,29 +1,29 @@
-//2人の名前と年齢を入力すると、その人たちの年齢の平均を表示するプログラムを書いてください。
 
+//2人の名前と年齢を入力すると、その人たちの年齢の平均を表示するプログラムを書いてください。
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Q3_2{
-  public static void main(String[] args){
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+public class Q3_2 {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-    try{
-      int number = 0;
-      int sum = 0;
+        try {
+            double sum = 0;
+            int number = 2;
 
-      for( ; number < 2; number++){
-        System.out.println( (number + 1) +"人目の名前を入力してください");
-        String name = reader.readLine();
-        System.out.println(name + "さんの年齢を入力してください。");
-        sum += Integer.parseInt(reader.readLine());
-      }
-      System.out.println(number + "人の平均年齢は" + (sum / number) + "です。");
-    }catch(IOException e){
-      System.out.println(e);
-    }catch(NumberFormatException e){
-      System.out.println("年齢が正しくありません");
+            for (int count = 0; count < number; count++) {
+                System.out.println((count + 1) + "人目の名前を入力してください");
+                String name = reader.readLine();
+                System.out.println(name + "さんの年齢を入力してください。");
+                sum += Integer.parseInt(reader.readLine());
+            }
+            System.out.println(number + "人の平均年齢は" + (sum / number) + "です。");
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (NumberFormatException e) {
+            System.out.println("年齢が正しくありません");
+        }
     }
-  }
 }
