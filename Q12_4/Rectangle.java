3b1b8eb7634620541404ee008b148a7a843d7cc8
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-05-02 15:13:52 +0900

    課題12-4 Rectangle.javaとPlacedRectangle.javaの作成

---
+++ Q12_4/Rectangle.java
@@ -0,0 +1,23 @@
+package Q12_4;
+class Rectangle {
+     int width;
+    int height;
+
+    Rectangle() {
+        setSize(0, 0);
+    }
+
+    Rectangle(int width, int height) {
+        setSize(width, height);
+    }
+
+    void setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + width + ", " + height + "]";
+    }
+}
