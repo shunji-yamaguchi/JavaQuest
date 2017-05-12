fcf151e6b4f56d90edc31dd63bd92d937ed11b62
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-05-12 15:40:15 +0900

    課題16-5 CountTen3.javaとそのテストコードの作成(未完)

---
+++ Q16_5/CountTen3.java
@@ -0,0 +1,63 @@
+package Q16_5;
+
+/**
+ * List16-1(CountTenA.java)では、runメソッドを実行しているのは1つのスレッドでした。
+ * 新たなスレッドを1つだけ生成するのではなく、
+ * ３個生成して動作するようにプログラムを書き換えてください。
+ * クラスの名前はCountTen3とします。
+ * その際、java.lang.Threadクラスには、
+ * 自分のスレッドの名前を得る次のメソッドがあります。
+ * これを利用して表示を行っているのがどのスレッドであるか分かるようにしてみましょう。
+ *     public final String getName()
+ * 必要なら、現在のスレッドを得る以下のスレッドも利用してください。
+ *     public static Thread currentThread()
+ *
+ * List16-1
+ * public class CoutnTenA extends Thread {
+ *     public static void main(String[] args) {
+ *         CountTenA ct = new CountTenA();
+ *         ct.start();
+ *         for (int i = 0; i < 10; i++){
+ *             System.out.println("main:i" + i);
+ *         }
+ *     }
+ *
+ *     @Override
+ *     public void run() {
+ *         for (int i = 0; i < 10; i++) {
+ *             System.out.println("run:i" + i);
+ *         }
+ *     }
+ * }
+*/
+
+public class CountTen3 extends Thread {
+    public static void main(String[] args) {
+        CountTen3 ct1 = new CountTen3();
+        CountTen3 ct2 = new CountTen3();
+        CountTen3 ct3 = new CountTen3();
+
+        ct1.start();
+        ct2.start();
+        ct3.start();
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread() + ":i" + i);
+        }
+        
+        try {
+            ct1.join();
+            ct2.join();
+            ct3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread() + ":i" + i);
+        }
+    }
+}
---
+++ Q16_5/CountTen3Test.java
@@ -0,0 +1,57 @@
+package Q16_5;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CountTen3Test {
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setup() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void teardown() {
+        System.setOut(null);
+        Thread.interrupted();
+
+    }
+
+    @Test
+    public void スレッドが動いてるか確認() throws InterruptedException {
+        CountTen3 ct = new CountTen3();
+        ct.start();
+        ct.join();
+        assertThat(output.toString(), endsWith(":i9" + System.lineSeparator()));
+    }
+
+    @Test
+    public void mainがスレッドを動かしたか確認() throws InterruptedException {
+        CountTen3.main(null);
+        CountTen3 ct3 = new CountTen3();
+        ct3.join();
+        //想定したスレッド名にならない
+        assertThat(output.toString(), containsString("Thread[Thread-0,5,main]:i9"));
+        assertThat(output.toString(), containsString("Thread[Thread-1,5,main]:i9"));
+        assertThat(output.toString(), containsString("Thread[Thread-2,5,main]:i9"));
+        
+    }
+
+    @Test
+    public void main自身が動いてるか確認() throws InterruptedException {
+        CountTen3.main(null);
+        CountTen3 ct3 = new CountTen3();
+        ct3.join();
+        assertThat(output.toString(), containsString("main]:i9"));
+    }
+
+
+}
