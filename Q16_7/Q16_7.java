fefcd6986bcdcf5cc195e09b3ef017b4198794f5
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-05-15 15:05:47 +0900

    Q16_7.java implementsを使わないよう変更

--- Q16_7/Q16_7.java
+++ Q16_7/Q16_7.java
@@ -6,8 +6,37 @@ package Q16_7;
 
 public class Q16_7 {
     public static void main(String[] args) {
-        PrintEveryThreeSecond p3 = new PrintEveryThreeSecond();
-        PrintEveryFiveSecond p5 = new PrintEveryFiveSecond();
+
+        Runnable p3 = new Runnable() {
+            @Override
+            public void run() {
+                String str = "***";
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        Thread.sleep(3000);
+                        System.out.println(str);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Runnable p5 = new Runnable() {
+            @Override
+            public void run() {
+                String str = "=====";
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        Thread.sleep(5000);
+                        System.out.println(str);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
         Thread thp3 = new Thread(p3);
         Thread thp5 = new Thread(p5);
 
@@ -17,46 +46,6 @@ public class Q16_7 {
         try {
             thp3.join();
             thp5.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-class PrintEveryThreeSecond implements Runnable {
-    private String str;
-
-    PrintEveryThreeSecond() {
-        str = "***";
-    }
-
-    @Override
-    public void run() {
-        try {
-            for (int i = 0; i < 10; i++) {
-                Thread.sleep(3000);
-                System.out.println(str);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
-class PrintEveryFiveSecond implements Runnable {
-    private String str;
-
-    PrintEveryFiveSecond() {
-        str = "=====";
-    }
-
-    @Override
-    public void run() {
-        try {
-            for (int i = 0; i < 10; i++) {
-                Thread.sleep(5000);
-                System.out.println(str);
-            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
