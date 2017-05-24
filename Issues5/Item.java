27f6c35747da3af258dd18afc0f2244e16a1d5ae
hironoriohashi <hironori.ohashi@esm.co.jp>
2017-05-24 10:43:44 +0900

    Item.javaの作成

---
+++ Issues5/Item.java
@@ -0,0 +1,10 @@
+
+public class Item {
+    public final String name;
+    public final int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
